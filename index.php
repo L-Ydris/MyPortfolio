0b2e3a5fc36f2a10f6e8d8919c2a43ce1ffbd9d9
LARIBI Ydris <62050952+L-Ydris@users.noreply.github.com>
2022-02-11 22:18:57 +0100

    demo php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Notre première instruction : echo</title>
-        <meta charset="utf-8" />
-    </head>
-    <body>
-        <h2>Affichage de texte avec PHP</h2>
-        
-        <p>
-            Cette ligne a été écrite entièrement en HTML.<br />
-            <?php echo("Celle-ci a été écrite entièrement en PHP."); ?>
-        </p>
-    </body>
-</html>
+<?php
+
+$prenom = 'Ydris';
+$nom = 'Doe';
+$note1 = 18;
+$note = 2;
+$moyenne = ($note1 + not2 ) / 2;
+
+echo 'Bonjour'. $prenom . ' ' . $nom . 'Vous avez eu' . $moyenne . 'de moyenne'; 
+
+
+?>
