cf01c9e3dd54cab332fe01888603048c5646e383
LARIBI Ydris <62050952+L-Ydris@users.noreply.github.com>
2022-02-16 21:39:15 +0100

    form

--- contact.php
+++ contact.php
@@ -3,5 +3,12 @@
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit facere maiores corrupti fuga,
     esse nisi nemo nesciunt saepe id quam cum exercitationem cupiditate.
     Ea doloribus explicabo sapiente praesentium nemo?
-
 </p>
+
+<form>
+    <div class="form-group">
+        <input type="text" name="prenom" placeholder="prénom">
+        <input type="text" name="nom" placeholder="nom">
+        <input type="email" name="email" placeholder="email">
+    </div>
+</form>
